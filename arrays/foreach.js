4b49eb9f9d1c71c2cb87d7ae5ee274e27f5c0a7e
Mehdi <semlali.mehdi.1980@gmail.com>
2021-04-28 22:09:12 +0000

    adding foreach

---
+++ arrays/foreach.js
@@ -0,0 +1,13 @@
+const data = require('./data')
+// for
+
+// for (let i = 0; i < companies.length; i++) {
+//     console.log(companies[i].name);
+// }
+
+// foreach
+
+data.companies.forEach(company => {
+    console.log(company.end);
+});
+

5804f7e669a7803eab56910f76d83725aaf02614
Mehdi <semlali.mehdi.1980@gmail.com>
2021-04-29 00:41:52 +0000

    Algothms: longest word 1

---
+++ algorithms/longestWord-1.js
@@ -0,0 +1,14 @@
+function longestWord(str) {
+    let words = str.split(' ');
+    let maxLength = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > maxLength) {
+            maxLength = words[i].length;
+        }
+    }
+    return maxLength;
+}
+
+
+console.log(longestWord("What is the longest word in the current text"));
